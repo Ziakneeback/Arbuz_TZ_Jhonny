bb4677aecb90ec0dc2c0a6948c91c28564e6f2b1
Zhanibek Asylbek <jonimoni84@gmail.com>
2024-05-17 21:38:44 +0500

    Initial Commit

---
+++ Arbuz_TZ_Jhonny/Arbuz_TZ_JhonnyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arbuz_TZ_JhonnyApp.swift
+//  Arbuz_TZ_Jhonny
+//
+//  Created by Жанибек Асылбек on 17.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Arbuz_TZ_JhonnyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
